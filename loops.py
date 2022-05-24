e87c26b213692b622439f5e81240bbbbd75a7c3f
sanmiao777 <81620786+sanmiao777@users.noreply.github.com>
2022-05-24 11:15:53 +0800

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,23 @@
+#
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+#a
+for i in range(0,101,10):
+    print(i, end=' ')
+print()
+#b
+for i in range(20,0,-1):
+    print(i, end=' ')
+print()
+#c
+number_of_stars = int(input("Number of stars: "))
+for i in range(number_of_stars):
+    print('*', end=' ')
+print()
+#d
+i = 1
+number_of_stars = int(input("Number of stars: "))
+while i < number_of_stars+1:
+    print('*' * i)
+    i += 1

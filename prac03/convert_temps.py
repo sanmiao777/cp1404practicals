babf0f8927b0208dfc037a22993d33ed223aedf7
sanmiao777 <81620786+sanmiao777@users.noreply.github.com>
2022-05-25 11:03:25 +0800

    Update convert_temps.py

--- prac03/convert_temps.py
+++ prac03/convert_temps.py
@@ -7,6 +7,8 @@ def main():
         input_file.write(str(fahrenheit)+'\n')
         celsius = f_to_c(fahrenheit)
         output_file.write(str(celsius)+'\n')
+    input_file.close()
+    output_file.close()
 def f_to_c(fahrenheit):
     return 5.0 * (fahrenheit - 32) / 9
 main()
